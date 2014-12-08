b4acf804ae83545c59f3fb0434c4fa1875f9dd8a
MaxParsons <maxwell.parsons@gmail.com>
2014-12-07 23:26:25 -0500

    Finishing up raman simulation code.

--- liexperiment/raman/rateequation_twolevel_1d_harmonic.py
+++ liexperiment/raman/rateequation_twolevel_1d_harmonic.py
@@ -6,93 +6,182 @@ Created on Dec 5, 2014
 import numpy as np
 import matplotlib.pyplot as plt
 from amo.core import odesolver
+from amo.core import physicalconstants
+from liexperiment.traps import harmonicoscillator3d
+c = physicalconstants.PhysicalConstantsSI
 
-if __name__ == "__main__":
-    # SI units everywhere
-    # experiment parameters
-    f_trap = 0.65e6# trap frequency
-    cutoff_state = 8.0# first state to tunnel, indexed from 0
-    pi_time = 2.0e-6# raman carrier pi-time
-    pump_time = 10.0e-6# pumping time
-    photons_per_raman = 3.0# recoil heating for fudge factor
+class RamanSimulation(object):
+    def __init__(self):
+        # SI units everywhere
+        # experiment parameters
+        self.f_trap = 0.65e6  # trap frequency
+        self.detuning = -self.f_trap
+        self.cutoff_state = 8.0  # first state to tunnel, indexed from 0
+        self.pi_time = 10.0e-6  # raman carrier pi-time
+        self.pump_time = 10.0e-6  # pumping time
+        self.photons_per_raman = 2.0  # recoil heating for fudge factor
+        self.duration = 0.5e-3  # how long is the raman cooling
+        self.numsteps = 500  # how many points for the simulation
     
-    # constants
-    F_RECOIL = 75.0e3
-    F_ANHARMONICITY = 26.0e3 
+        # constants
+        self.f_recoil = 75.0e3
+        self.f_anharmonicity = 26.0e3 
+    
     
     # calculated from experiment parameters
-    eta_0 = np.sqrt(F_RECOIL / f_trap)
-    eta = 0.19 * np.sqrt(1.0e6 / f_trap)
-    eps = photons_per_raman * eta_0 ** 2
+    @property
+    def cutoff_state(self):
+        return self._cutoff_state
+    @cutoff_state.setter
+    def cutoff_state(self, value):
+        self._cutoff_state = value
+        self.init_populations = np.zeros((self._cutoff_state + 1,))
+        self.transition_matrix = np.zeros((self._cutoff_state + 1, self._cutoff_state + 1))
+        
+        
+        
+    @property
+    def eta_0(self):
+        return np.sqrt(self.f_recoil / self.f_trap)
+    @property
+    def eta(self):
+        return 0.19 * np.sqrt(1.0e6 / self.f_trap)
+    @property
+    def eps(self):
+        return self.photons_per_raman * self.eta_0 ** 2
     
-    # SIMULATION
-    # setup arrays
-    init_populations = np.zeros((cutoff_state + 1,))
-    init_populations[0] = 0.5
-    init_populations[2] = 0.5
-    transition_matrix = np.zeros((cutoff_state + 1, cutoff_state + 1))
-    
-    def lineshape(detuning, raman_rabi, decay_rate):
+    def lineshape(self, detuning, raman_rabi, decay_rate):
         s0 = 2.0 * raman_rabi ** 2 / decay_rate ** 2
         return decay_rate / 2.0 * s0 / (1 + s0 + (2.0 * detuning / decay_rate) ** 2)
     
-    def sideband_frequencies(n0, nf):
+    def sideband_frequencies(self, n0, nf):
         if n0 < nf:
-            return f_trap * (nf - n0) - np.sign(nf - n0) * F_ANHARMONICITY * np.sum(np.arange(n0, nf))
+            return self.f_trap * (nf - n0) - np.sign(nf - n0) * self.f_anharmonicity * np.sum(np.arange(n0, nf))
         else:
-            return f_trap * (nf - n0) - np.sign(nf - n0) * F_ANHARMONICITY * np.sum(np.arange(nf, n0))
+            return self.f_trap * (nf - n0) - np.sign(nf - n0) * self.f_anharmonicity * np.sum(np.arange(nf, n0))
     
-    def transition_rate_neighbor_only(n0, nrate, detuning):
-        decay_rate = 1.0 / pump_time
-        if nrate == cutoff_state:
-            if (nrate - n0) == 1:
-                up_rabi = np.sqrt(n0 + 1) * eta * np.pi / pi_time
-                stay_rabi = np.pi / pi_time
-                f_heat = sideband_frequencies(n0, n0 + 1)
-                return (1 + eps) * (lineshape(detuning - f_heat, up_rabi, decay_rate) + eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate))
-            elif nrate == n0:
-                down_rabi = np.sqrt(n0) * eta * np.pi / pi_time
-                up_rabi = np.sqrt(n0 + 1) * eta * np.pi / pi_time
-                stay_rabi = np.pi / pi_time
-                f_heat = sideband_frequencies(n0, n0 + 1)
-                f_cool = sideband_frequencies(n0, n0 - 1)
-                return (1 - eps) * (-lineshape(detuning - f_cool, down_rabi, decay_rate) - eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate))
+    def transition_rate_neighbor_only(self, n0, nrate):
+        decay_rate = 2.0 * np.pi / self.pump_time
+        if nrate == self._cutoff_state:
+            if (nrate - n0) == 1:  # heating from state below
+                up_rabi = np.sqrt(n0 + 1) * self.eta * np.pi / self.pi_time
+                stay_rabi = np.pi / self.pi_time
+                f_heat = self.sideband_frequencies(n0, n0 + 1)
+                return (1 + self.eps) * (self.lineshape(self.detuning - f_heat, up_rabi, decay_rate) + self.eta ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
+            elif nrate == n0: 
+                return 0.0
             else:
                 return 0.0
-        else:
+        elif nrate == self._cutoff_state - 1:
             if (nrate - n0) == 1:
-                up_rabi = np.sqrt(n0 + 1) * eta * np.pi / pi_time
-                stay_rabi = np.pi / pi_time
-                f_heat = sideband_frequencies(n0 , n0 + 1)
-                return (1 + eps) * (lineshape(detuning - f_heat, up_rabi, decay_rate) + eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate))
+                up_rabi = np.sqrt(n0 + 1) * self.eta * np.pi / self.pi_time
+                stay_rabi = np.pi / self.pi_time
+                f_heat = self.sideband_frequencies(n0 , n0 + 1)
+                return (1 + self.eps) * (self.lineshape(self.detuning - f_heat, up_rabi, decay_rate) + self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
             elif (nrate - n0) == -1:
-                down_rabi = np.sqrt(n0) * eta * np.pi / pi_time
-                stay_rabi = np.pi / pi_time
-                f_cool = sideband_frequencies(n0, n0 - 1)
-                return (1 - eps) * (lineshape(detuning - f_cool, down_rabi, decay_rate) + eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate))
+                return 0.0
             elif nrate == n0:
-                down_rabi = np.sqrt(n0) * eta * np.pi / pi_time
-                up_rabi = np.sqrt(n0 + 1) * eta * np.pi / pi_time
-                stay_rabi = np.pi / pi_time
-                f_heat = sideband_frequencies(n0, n0 + 1)
-                f_cool = sideband_frequencies(n0, n0 - 1)
+                down_rabi = np.sqrt(n0) * self.eta * np.pi / self.pi_time
+                up_rabi = np.sqrt(n0 + 1) * self.eta * np.pi / self.pi_time
+                stay_rabi = np.pi / self.pi_time
+                f_heat = self.sideband_frequencies(n0, n0 + 1)
+                f_cool = self.sideband_frequencies(n0, n0 - 1)
+                return (1 + self.eps) * (-self.lineshape(self.detuning - f_heat, up_rabi, decay_rate) - self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate)) + \
+                        (1 - self.eps) * (-self.lineshape(self.detuning - f_cool, down_rabi, decay_rate) - self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
+            else:
+                return 0.0
+        else:
+            if (nrate - n0) == 1:
+                up_rabi = np.sqrt(n0 + 1) * self.eta * np.pi / self.pi_time
+                stay_rabi = np.pi / self.pi_time
+                f_heat = self.sideband_frequencies(n0 , n0 + 1)
+                return (1 + self.eps) * (self.lineshape(self.detuning - f_heat, up_rabi, decay_rate) + self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
+            elif (nrate - n0) == -1:
+                down_rabi = np.sqrt(n0) * self.eta * np.pi / self.pi_time
+                stay_rabi = np.pi / self.pi_time
+                f_cool = self.sideband_frequencies(n0, n0 - 1)
+                return (1 - self.eps) * (self.lineshape(self.detuning - f_cool, down_rabi, decay_rate) + self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
+            elif nrate == n0:
+                down_rabi = np.sqrt(n0) * self.eta * np.pi / self.pi_time
+                up_rabi = np.sqrt(n0 + 1) * self.eta * np.pi / self.pi_time
+                stay_rabi = np.pi / self.pi_time
+                f_heat = self.sideband_frequencies(n0, n0 + 1)
+                f_cool = self.sideband_frequencies(n0, n0 - 1)
                 if n0 == 0:
-                    return (1 + eps) * (-lineshape(detuning - f_heat, up_rabi, decay_rate) - eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate))
+                    return (1 + self.eps) * (-self.lineshape(self.detuning - f_heat, up_rabi, decay_rate) - self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
                 else:
-                    return (1 + eps) * (-lineshape(detuning - f_heat, up_rabi, decay_rate) - eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate)) + \
-                        (1 - eps) * (-lineshape(detuning - f_cool, down_rabi, decay_rate) - eta_0 ** 2 * lineshape(detuning, stay_rabi, decay_rate))
+                    return (1 + self.eps) * (-self.lineshape(self.detuning - f_heat, up_rabi, decay_rate) - self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate)) + \
+                        (1 - self.eps) * (-self.lineshape(self.detuning - f_cool, down_rabi, decay_rate) - self.eta_0 ** 2 * self.lineshape(self.detuning, stay_rabi, decay_rate))
             else:
                 return 0.0
     
-    for n0 in np.arange(0, len(init_populations)):
-        for nrate in np.arange(0, len(init_populations)):
-            transition_matrix[nrate, n0] = transition_rate_neighbor_only(n0, nrate, -f_trap)
-
-    print np.sum(transition_matrix, 0)
-    r = odesolver.rateequation(transition_matrix, init_populations)
-    r.solve(1.0e-3, numsteps=50)
-    r.result.plot()
+    def set_transition_matrix(self):
+        for n0 in np.arange(0, len(self.init_populations)):
+            for nrate in np.arange(0, len(self.init_populations)):
+                self.transition_matrix[nrate, n0] = self.transition_rate_neighbor_only(n0, nrate)
+                
+    def set_mean_phonon_number(self):
+        numbers = np.arange(0, self._cutoff_state + 1)
+        self.mean_phonon_number = np.dot(self.populations, numbers)
+        
+    def simulate_cooling(self, isPlot=False):
+        self.set_transition_matrix()
+    
+        # print np.sum(self.transition_matrix, 0)
+        r = odesolver.rateequation(self.transition_matrix, self.init_populations)
+        r.solve(self.duration, numsteps=self.numsteps)
+        self.populations = r.result.populations
+        self.times = r.result.t
+        self.set_mean_phonon_number()
+        if isPlot:
+            r.result.plot()
+        
+if __name__ == "__main__":
+    Er = c.h * 75.0e3
+    sim = RamanSimulation()
+    average_energy = 3.0 * Er
+    sim.f_trap = 1.0e6  # trap frequency
+    sim.cutoff_state = 8.0  # first state to tunnel, indexed from 0
+    trap_frequencies = np.array([sim.f_trap, 950.0e3, 1300.0e3])
+    cutoffs = np.array([sim._cutoff_state, 7, 11])   
+    sim.detuning = -sim.f_trap
+    sim.pi_time = 2.0e-6  # raman carrier pi-time
+    sim.pump_time = 10.0e-6  # pumping time
+    sim.photons_per_raman = 2.0  # recoil heating for fudge factor
+    sim.duration = 0.2e-3  # how long is the raman cooling
+    sim.numsteps = 100  # how many points for the simulation
+    
+    
+    lat = harmonicoscillator3d.harmonicoscillator3d(trap_frequencies, cutoffs)
+    print lat.cutoffs[0]
+    print sim.cutoff_state
+    print sim.init_populations.shape
+    temperature = lat.temperature(average_energy)
+    
+    sim.init_populations[0:-1] = lat.populations_sum_over_all_but_first_frequency(temperature)
+    sim.simulate_cooling(isPlot=True)
+    
+    figmean, axmean = plt.subplots(1, 1)
+    figgnd, axgnd = plt.subplots(1, 1)
+    figlost, axlost = plt.subplots(1, 1)
+    
+    axmean.plot(sim.times, sim.mean_phonon_number, marker = "o", linestyle = "None")
+    axmean.set_title("Mean phonon number vs. Cooling Time")
+    axmean.set_ylabel("Mean phonon number")
+    axmean.set_xlabel("Cooling time (s)")
+    
+    axgnd.plot(sim.times, sim.populations[:, 0], marker = "o", linestyle = "None")
+    axgnd.set_title("Ground state fraction vs. Cooling Time")
+    axgnd.set_ylabel("Ground state fraction")
+    axgnd.set_xlabel("Cooling time (s)")
+    
+    axlost.plot(sim.times, sim.populations[:, sim._cutoff_state], marker = "o", linestyle = "None")
+    axlost.set_title("Fraction lost vs. Cooling Time")
+    axlost.set_ylabel("Fraction lost")
+    axlost.set_xlabel("Cooling time (s)")
+    
     plt.show()
+    
     
     
 
--- liexperiment/traps/harmonicoscillator3d.py
+++ liexperiment/traps/harmonicoscillator3d.py
@@ -6,6 +6,7 @@ Created on Nov 27, 2014
 import numpy as np
 import matplotlib.pyplot as plt
 import amo.core.physicalconstants
+import scipy.optimize
 c = amo.core.physicalconstants.PhysicalConstantsSI
 
 class harmonicoscillator3d(object):
@@ -26,6 +27,11 @@ class harmonicoscillator3d(object):
         return np.sum(c.h * self.frequencies * \
                (np.exp(beta * c.h * self.frequencies) - 1) ** (-1))
         
+    def temperature(self, energy):
+        def _average_energy_zero(temperature):
+            return self.average_energy(temperature) - energy
+        return scipy.optimize.brentq(_average_energy_zero, 0.1 * energy/(c.kb), 2.0 * energy/(c.kb))
+        
     def population(self, states, temperature):
         beta = (c.kb * temperature) ** (-1)
         return np.product(np.exp(-beta * c.h * self.frequencies * (states + 0.5)))\
@@ -34,6 +40,19 @@ class harmonicoscillator3d(object):
     def population_sum_over_first_frequency(self, states, temperature):
         return np.sum([self.population(np.array([n, states[0], states[1]]), temperature) for n in range(0, self.cutoffs[0])])
         pass
+    
+    def population_sum_over_all_but_first_frequency(self, state, temperature):
+        sum = 0.0
+        for n1 in np.arange(0, self.cutoffs[1]):
+            for n2 in np.arange(0, self.cutoffs[2]):
+                sum += self.population(np.array([state, n1, n2]), temperature)
+        return sum
+    
+    def populations_sum_over_all_but_first_frequency(self, temperature):
+        pops = np.zeros((self.cutoffs[0],))
+        for n in np.arange(0, self.cutoffs[0]):
+            pops[n] = self.population_sum_over_all_but_first_frequency(n, temperature)
+        return pops
     
     def atoms_remaining(self, temperature):
         atoms = 0
@@ -49,13 +68,15 @@ if __name__ == '__main__':
     frequencies = np.array([650.0e3, 950.0e3, 1300.0e3])
     cutoffs = np.array([49, 7, 11])   
     lat = harmonicoscillator3d(frequencies, cutoffs)
-    temperatures = np.linspace(0.01 * Tr, 500 * Tr)
+    temperatures = np.linspace(0.01 * Tr, 30 * Tr)
     energies = [lat.average_energy(temp) for temp in temperatures]
     populations = [lat.population(np.array([0, 0, 0]), temp) for temp in temperatures]
     radial_pops = [lat.population_sum_over_first_frequency(np.array([0, 0]), temp) for temp in temperatures]
     atoms_remaining = [lat.atoms_remaining(temp) for temp in temperatures]
     energies = np.array(energies)
-    
-    plt.plot(energies / Er, atoms_remaining, marker='o')
+     
+    #plt.plot(energies / Er, atoms_remaining, marker='o')
+    plt.plot(energies / Er, c.kb * temperatures / Er)
+    print lat.temperature(20.0 * Er)*c.kb / Er
     plt.show()
         
